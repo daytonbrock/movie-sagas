3bc5105dd58885dc4fb20b7a3c6adb1efa562c93
dayton brock <dayton.brock@gmail.com>
2019-06-23 18:39:40 -0500

    add GET route for one movie

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -5,13 +5,26 @@ const router = express.Router();
 
 // GET all movies from database
 router.get('/', (req, res) => {
-    const queryText = 'SELECT * FROM "movies";'
+    const queryText = 'SELECT * FROM "movies";';
     pool.query(queryText)
     .then(response => {
         console.log(response);
         res.send(response.rows);
     }).catch(error => {
         console.log('error with SELECT on /api/movies route:', error);
+        res.sendStatus(500);
+    }); // end pool query
+}); // end get
+
+// GET one movie to view in details page
+router.get('/details/:id', (req, res) => {
+    const queryText = 'SELECT * FROM "movies" WHERE "id"=$1;';
+    pool.query(queryText, [req.params.id])
+    .then(response => {
+        console.log(response);
+        res.send(response.rows);
+    }).catch(error => {
+        console.log('error with SELECT on /api/movies/details route:', error);
         res.sendStatus(500);
     }); // end pool query
 }); // end get
