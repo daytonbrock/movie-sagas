430ece39ffd332b310773d6eda9632480678bddd
dayton brock <dayton.brock@gmail.com>
2019-06-24 00:10:38 -0500

    build out edit page, add TextFields for editing title and description

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,14 +1,80 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class Edit extends Component {
+
+    componentDidMount() {
+        const movieId = this.props.match.params.movieId;
+        this.props.dispatch({
+            type: 'FETCH_ONE_MOVIE', 
+            payload: movieId,
+        });
+        this.props.dispatch({
+            type: 'FETCH_ONE_MOVIE_GENRES',
+            payload: movieId,
+        });
+    }
+
+    updateMovie = () => {
+        console.log('save changes');
+    }
+
     render() {
         return (
-            <div>
-                <p>display edit page for one movie here</p>
+            <div className="App">
+                <Grid container
+                    justify='center'
+                    alignContent='center'>
+                    <Grid item xs={3}>
+                        <Button onClick={() => this.props.history.push(`/details/${this.props.match.params.movieId}`)}>
+                            Cancel
+                        </Button>
+                        <Button onClick={this.updateMovie}>
+                            Save
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            fullWidth margin="normal"
+                            multiline rowsMax="20"
+                            label="Title"
+                            defaultValue={this.props.reduxState.oneMovie.title}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <h4>Genres:</h4>
+                    </Grid>
+                </Grid>
+                <Grid container>
+                        <Grid item xs={3}>
+                            <img src={this.props.reduxState.oneMovie.poster} 
+                                alt={this.props.reduxState.oneMovie.title}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            {/* <p>{this.props.reduxState.oneMovie.description}</p> */}
+                            <TextField
+                                fullWidth margin="normal"
+                                multiline rowsMax="20"
+                                label="Description"
+                                defaultValue={this.props.reduxState.oneMovie.description}
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            {this.props.reduxState.oneMovieGenres.map(genre => <div key={genre.id}>{genre.name}</div>)}
+                        </Grid>
+                </Grid>
             </div>
         );
     }
 }
 
-export default connect()(Edit);
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState,
+});
+
+export default connect(mapReduxStateToProps)(Edit);
