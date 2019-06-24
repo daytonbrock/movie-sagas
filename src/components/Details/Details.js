c591ce8d560feaad9998f337830d3ca22c1da0bf
dayton brock <dayton.brock@gmail.com>
2019-06-23 23:53:52 -0500

    add navigation to details page and update font-family

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -26,15 +26,10 @@ class Details extends Component {
                     justify='center'
                     alignContent='center'>
                     <Grid item xs={3}>
-                        {/* <IconButton>
-                            <SvgIcon>
-                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-                            </SvgIcon>
-                        </IconButton> */}
-                        <Button>
+                        <Button onClick={() => this.props.history.push('/')}>
                             Back to List
                         </Button>
-                        <Button>
+                        <Button onClick={() => this.props.history.push(`/edit/${this.props.match.params.movieId}`)}>
                             Edit
                         </Button>
                     </Grid>
@@ -42,6 +37,7 @@ class Details extends Component {
                         <h3>{this.props.reduxState.oneMovie.title}</h3>
                     </Grid>
                     <Grid item xs={3}>
+                        <h4>Genres:</h4>
                     </Grid>
                 </Grid>
                 <Grid container>
@@ -53,7 +49,6 @@ class Details extends Component {
                             <p>{this.props.reduxState.oneMovie.description}</p>
                         </Grid>
                         <Grid item xs={3}>
-                            <h4>Genres:</h4>
                             {this.props.reduxState.oneMovieGenres.map(genre => <div key={genre.id}>{genre.name}</div>)}
                         </Grid>
                 </Grid>
