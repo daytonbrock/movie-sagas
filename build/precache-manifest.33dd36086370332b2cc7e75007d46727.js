9e530bfa0f42a34aa91bce4b4bb5862dd1b8117a
dayton brock <dayton.brock@gmail.com>
2019-08-24 10:29:08 -0500

    add favicon, styling to images on homepage, new build

---
+++ build/precache-manifest.33dd36086370332b2cc7e75007d46727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a38de9f4f2f2961ff2d9197b6b0989",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283c750374f063ecc8ba",
+    "url": "/static/css/main.d069c7be.chunk.css"
+  },
+  {
+    "revision": "158f890613beaf90e39a",
+    "url": "/static/js/2.54beeb6a.chunk.js"
+  },
+  {
+    "revision": "283c750374f063ecc8ba",
+    "url": "/static/js/main.6cfccf04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
